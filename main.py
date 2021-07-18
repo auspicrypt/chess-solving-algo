5cfede2ac12245e46e292d31034b1dcfc518b6a5
auspicrypt <saroha93@gmail.com>
2021-07-18 08:53:20 +0200

    Initial python files

---
+++ main.py
@@ -0,0 +1,26 @@
+#all the imports
+
+#all the functions
+
+#defining all the pieces
+
+#def pawn():
+
+#def rook():
+
+#def knight():
+
+#def bishop():
+
+#def king():
+
+#def queen():
+
+chess_board=range(64)
+
+print(chess_board)
+
+
+#main funcation
+#if __name__=="__main__":
+
